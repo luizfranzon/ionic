325d49a05d892473305d98bce6ffd652a7438d2f
Luiz Fernando Franzon <luiz.franzon@linkedrh.com.br>
2024-10-10 12:42:25 -0300

    run: android studio setuop

--- 02-ionic-angular/capacitor.config.ts
+++ 02-ionic-angular/capacitor.config.ts
@@ -1,9 +1,9 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.luizfranzon.ionic-angular',
   appName: '02-ionic-angular',
-  webDir: 'www'
+  webDir: 'www',
 };
 
 export default config;
