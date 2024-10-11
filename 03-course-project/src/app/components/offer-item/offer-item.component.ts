2e3e9e32dfb634811cd03fbca2a2238b85de70af
Luiz Fernando Franzon <luiz.franzon@linkedrh.com.br>
2024-10-11 15:59:33 -0300

    add: app theme and create offer-item component

---
+++ 03-course-project/src/app/components/offer-item/offer-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, input, OnInit } from '@angular/core';
+import { Place } from 'src/app/models/place.model';
+
+@Component({
+  selector: 'app-offer-item',
+  templateUrl: './offer-item.component.html',
+  styleUrls: ['./offer-item.component.scss'],
+})
+export class OfferItemComponent {
+  offer = input.required<Place>();
+
+  getDate() {
+    return new Date();
+  }
+}
